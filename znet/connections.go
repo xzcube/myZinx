87e47488428fdfd2b2c62f1a28a0c37551f670c0
xzcube <1482240153@qq.com>
2022-04-20 21:49:36 +0800

    开始方法

--- znet/connections.go
+++ znet/connections.go
@@ -27,11 +27,26 @@ type Connection struct {
 }
 
 func (c *Connection) Start() {
-	panic("implement me")
+	fmt.Println("Conn Start()... ConnID is", c.ConnID)
+	// 启动从当前连接的读数据业务
+	go c.StartReader()
+
+	// TODO 启动从当前链接写数据的业务
+
 }
 
 func (c *Connection) Stop() {
 	fmt.Println("Conn Stop.. ConnID =", c.ConnID)
+
+	// 如果当前链接已经关闭
+	if c.IsClosed {
+		return
+	}
+	c.IsClosed = false
+
+	// 关闭socket链接
+	c.Conn.Close()
+	close(c.ExitChan)
 }
 
 func (c *Connection) GetConnection() *net.TCPConn {
@@ -63,3 +78,27 @@ func NewConnection(conn *net.TCPConn, connID uint32, callbackApi ziface.HandleFu
 	return c
 }
 
+func (c *Connection) StartReader() {
+	fmt.Println("Reader Goroutine is running")
+
+	defer fmt.Println("connID = ", c.ConnID, "Reader is exit, remote addr is", c.GetRemoteAddr().String())
+	defer c.Stop()
+
+	for {
+		// 读取客户端的数据到buff中，目前最大为512字节
+		buf := make([]byte, 512)
+		cnt, err := c.Conn.Read(buf)
+		if err != nil {
+			fmt.Println("recv buff err:", err)
+			continue
+		}
+
+		// 调用当前连接所绑定的handleAPI
+		err = c.HandleAPI(c.Conn, buf, cnt)
+		if err != nil {
+			fmt.Println("ConnID:", c.ConnID, "handle is error:", err)
+			break
+		}
+	}
+
+}
